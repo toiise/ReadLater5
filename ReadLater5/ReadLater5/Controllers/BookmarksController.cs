e9c8261171067084d679006525c3a30950cc6705
Todor <todor@isolutions.mk>
2021-07-30 15:12:37 +0200

    Add project files.

---
+++ ReadLater5/ReadLater5/Controllers/BookmarksController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entity;
+using Services;
+
+namespace ReadLater5.Controllers
+{
+    public class BookmarksController : Controller
+    {
+
+         IBookmarkService _iBookmarkService;
+
+         public BookmarksController(IBookmarkService bookmarkService)
+         {
+             _iBookmarkService = bookmarkService;
+         }
+         // GET: Bookmarks
+        public IActionResult Index()
+        {
+            List<Bookmark> model = _iBookmarkService.GetBookmarks();
+            return View(model);
+        }
+
+        // GET: Bookmarks/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+            Bookmark bookmark = _iBookmarkService.GetBookmarkById((int)id);
+            if (bookmark == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            return View(bookmark);
+
+        }
+
+        // GET: Bookmarks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bookmarks/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create( Bookmark bookmark)
+        {
+            if (ModelState.IsValid)
+            {
+                _iBookmarkService.CreateBookmark(bookmark);
+                return RedirectToAction("Index");
+            }
+
+            return View(bookmark);
+        }
+
+        // GET: Bookmarks/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+            Bookmark bookmark = _iBookmarkService.GetBookmarkById((int)id);
+            if (bookmark == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            return View(bookmark);
+        }
+
+        // POST: Bookmarks/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Bookmark bookmark)
+        {
+            if (ModelState.IsValid)
+            {
+                _iBookmarkService.UpdateBookmark(bookmark);
+                return RedirectToAction("Index");
+            }
+            return View(bookmark);
+        }
+
+        // GET: Bookmarks/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+            Bookmark bookmark = _iBookmarkService.GetBookmarkById((int)id);
+            if (bookmark == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            return View(bookmark);
+        }
+
+        // POST: Bookmarks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Bookmark bookmark = _iBookmarkService.GetBookmarkById(id);
+            _iBookmarkService.DeleteBookmark(bookmark);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Services/Services/BookmarkService.cs
@@ -0,0 +1,48 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Services
+{
+    public class BookmarkService : IBookmarkService
+    {
+        private ReadLaterDataContext _ReadLaterDataContext;
+
+        public BookmarkService(ReadLaterDataContext readLaterDataContext)
+        {
+            _ReadLaterDataContext = readLaterDataContext;
+        }
+        public Bookmark CreateBookmark(Bookmark bookmark)
+        {
+            _ReadLaterDataContext.Add(bookmark);
+            _ReadLaterDataContext.SaveChanges();
+            return bookmark;
+        }
+
+        public void DeleteBookmark(Bookmark bookmark)
+        {
+            _ReadLaterDataContext.Bookmark.Remove(bookmark);
+            _ReadLaterDataContext.SaveChanges();
+        }
+
+        public Bookmark GetBookmarkById(int Id)
+        {
+            return _ReadLaterDataContext.Bookmark.Where(c => c.ID == Id).FirstOrDefault();
+        }
+
+        public List<Bookmark> GetBookmarks()
+        {
+            return _ReadLaterDataContext.Bookmark.ToList();
+        }
+
+        public void UpdateBookmark(Bookmark bookmark)
+        {
+            _ReadLaterDataContext.Update(bookmark);
+            _ReadLaterDataContext.SaveChanges();
+        }
+    }
+}
